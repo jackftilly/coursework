758a13235aab5a9735df69547759ed77318534a3
App Academy Student <student@appacademy.io>
2016-07-15 16:21:39 -0700

    giving up

--- tictactoeai/tic_tac_toe_ai/skeleton/lib/super_computer_player.rb
+++ tictactoeai/tic_tac_toe_ai/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,27 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board,mark)
+    node.children.each
+    winning_moves = []
+    losing_moves = []
+    node.children.each do |x|
+      if x.winning_node?(mark) && x.board.winner == mark
+        winning_moves = x.prev_move_pos
+        break
+      elsif x.winning_node?(mark)
+        winning_moves << x.prev_move_pos
+      end
+    end
+
+    node.children.each do |x|
+      if x.losing_node?(mark)
+        losing_moves << x.prev_move_pos
+      end
+    end
+
+    winning_moves - losing_moves
+
   end
 end
 
--- tictactoeai/tic_tac_toe_ai/skeleton/lib/tic_tac_toe_node.rb
+++ tictactoeai/tic_tac_toe_ai/skeleton/lib/tic_tac_toe_node.rb
@@ -8,6 +8,7 @@ class TicTacToeNode
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
     @child = []
+    @depth = 0
   end
 
   def losing_node?(evaluator)
@@ -63,9 +64,6 @@ class TicTacToeNode
     false
   end
 
-
-  # This method generates an array of all moves that can be made after
-  # the current move.
   def children
     3.times do |row|
       3.times do |col|
